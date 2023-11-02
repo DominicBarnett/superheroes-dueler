9c61665882367db9b675505d1e907fda82f9e729
DominicBarnett <dominic.barnett@icloud.com>
2023-11-02 14:31:52 -0700

    Initial commit

---
+++ dog.py
@@ -0,0 +1,16 @@
+# dog.py
+class Dog:
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+        print("dog initialized!")
+
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print(f'{self.name} sits')
+    
+    def roll(self):
+        print(f'{self.name} rolls over')
+      
---
+++ my_dogs.py
@@ -0,0 +1,12 @@
+# my_dogs.py
+import dog # we need to specify exactly what we want
+
+my_dog = dog.Dog("Rex", "SuperDog")
+my_dog.bark()
+my_other_dog = dog.Dog("Annie", "SuperDog")
+Dom_dog1 = dog.Dog("Tito", "Brawlin' Boxer")
+Dom_dog2 = dog.Dog("Choco", "Monstrous American Bully")
+Dom_dog3 = dog.Dog("Dior", "Monstrous American Bully")
+Dom_dog1.bark()
+Dom_dog2.sit()
+Dom_dog3.roll()
